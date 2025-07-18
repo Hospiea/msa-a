8a96596cdc756794a7324c20fb9a8eaea5e3940b
Hospiea <155417815+Hospiea@users.noreply.github.com>
2025-07-19 07:57:23 +0900

    asdf

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+  kotlin("jvm") version "1.9.25"
+  kotlin("plugin.spring") version "1.9.25"
+  id("org.springframework.boot") version "3.5.3"
+  id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "org.com"
+version = "0.0.1-SNAPSHOT"
+
+java {
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(17)
+  }
+}
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  implementation ("org.springframework.boot:spring-boot-starter-web")
+  implementation("org.springframework.boot:spring-boot-starter-amqp")
+  implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+  implementation("org.jetbrains.kotlin:kotlin-reflect")
+  implementation("org.springframework.kafka:spring-kafka")
+  runtimeOnly("com.h2database:h2")
+  runtimeOnly("com.mysql:mysql-connector-j")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+  testImplementation("org.springframework.amqp:spring-rabbit-test")
+  testImplementation("org.springframework.kafka:spring-kafka-test")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+  compilerOptions {
+    freeCompilerArgs.addAll("-Xjsr305=strict")
+  }
+}
+
+tasks.withType<Test> {
+  useJUnitPlatform()
+}
---
+++ src/main/kotlin/org/com/kafka/controller/HomeController.kt
@@ -0,0 +1,17 @@
+package org.com.kafka.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("")
+class HomeController (
+
+) {
+
+  @GetMapping("/")
+  fun home() {
+    println("Hello")
+  }
+}
